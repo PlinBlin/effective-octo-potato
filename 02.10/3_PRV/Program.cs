8bae6c87c3963508bb148966ec61fc796d65489a
PlinBlin <zdrkqth8afn5@mail.ru>
2023-10-02 19:31:14 +0700

    3

---
+++ 02.10/3_PRV/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int a1 = int.Parse(Console.ReadLine());
+        int b1 = int.Parse(Console.ReadLine());
+        int a2 = int.Parse(Console.ReadLine());
+        int b2 = int.Parse(Console.ReadLine());
+
+        int sStart = Math.Max(a1, a2);
+        int eEnd = Math.Min(b1, b2);
+
+        if (sStart <= eEnd)
+            Console.WriteLine(sStart + " " + eEnd);
+        else
+            Console.WriteLine("пустое множество");
+    }
+}
